c552e52e1d884962a218a15fdae905994445eb6e
William Zamora <wzamora@mail.csuchico.edu>
2017-04-25 19:54:20 -0700

    Finished

--- gallery.js
+++ gallery.js
@@ -1,6 +1,11 @@
-
-
+var chico = ["Downtown", "Bulldog Taqueria", "Lower Bidwell", "Upper Bidwell","O'Connell"];
+var current;
 function shuffleGallery()
 {
-
+  var randI;
+  do {
+    randI = Math.floor(Math.random() * chico.length);
+  } while (randI == current);
+  current = randI;
+  console.log(chico[current]);
 }
